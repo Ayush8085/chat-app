77711dc554cefbf5db90755f5fd55b36fee77ea8
Ayush <ayushkumar9601@gmail.com>
2024-02-11 19:46:54 +0530

    logout complete

--- frontend/src/components/sidebar/LogoutBtn.jsx
+++ frontend/src/components/sidebar/LogoutBtn.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BiLogOut } from 'react-icons/bi'
+import { useAuthContext } from '../../hooks/useAuth';
+import {toast} from 'react-hot-toast';
+import { BACKEND_URI } from '../../App';
 
 const LogoutBtn = () => {
+    const [isLoading, setIsloading] = useState(false);
+    const { setAuthUser } = useAuthContext();
+
+    const handleLogout = async () => {
+        setIsloading(true);
+        try {
+            const response = await fetch(`${BACKEND_URI}/api/v1/auth/logout`, {
+                headers: { 'Content-Type': 'application/json' }
+            })
+            const data = await response.json();
+            if (data.error) {
+                throw new Error(data.error);
+            }
+            localStorage.removeItem('auth-user');
+            setAuthUser(null);
+        } catch (error) {
+            return toast.error(error);
+        } finally {
+            setIsloading(false);
+        }
+    }
+
     return (
         <div>
-            <BiLogOut className='w-7 h-7 text-slate-500 cursor-pointer' />
+            <BiLogOut className='w-7 h-7 text-slate-500 cursor-pointer' onClick={handleLogout} />
         </div>
     )
 }
