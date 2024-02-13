fed5db4c4c814402a463b95aa4fe4b8ff184df82
Ayush <ayushkumar9601@gmail.com>
2024-02-14 00:06:34 +0530

    minor change

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ import { app, server } from "./socket/socket.js";
 // ------------- MIDDLEWARES -------------
 app.use(
   cors({
-    origin: [FRONTEND_URL, 'http://localhost:5173'],
+    origin: ['https://chat-app-ayush8085.vercel.app', 'http://localhost:5173'],
     credentials: true,
   }),
 );
